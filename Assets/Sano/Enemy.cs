ec9bedd88bee068d309b0920bea1779fc43c8d9b
Junpei Sano <s2010335@edu.cc.uec.ac.jp>
2024-12-20 15:08:41 +0900

    minor update

--- Assets/Sano/Enemy.cs
+++ Assets/Sano/Enemy.cs
@@ -3,22 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
+/* メモ
+ * ・最初はdisableにしてinstantiate, OnEnableで動き始めるようにする
+ * 
+ * */
+
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] private Camera m_camera;
+
     // とりあえずprivate
     private int hp = 1;
     private float speed = 1.0f;
     private float attack = 1.0f;
 
-    // スポーンの場所
-    protected Vector2 spawnPosition;
 
     // Trueの間ターゲットされているマークを表示するようにする
     public bool isTargeting = false;
 
+    private bool isAttacked = false;    // trueの間敵と衝突してもAttackPlayerを呼ばない
     private Rigidbody2D m_rig;
 
+
+    private void OnEnable()
+    {
+        Spawn();
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -32,15 +43,13 @@ public class Enemy : MonoBehaviour
         Move();
     }
 
-    private void SetSpawnPosition()
+    // オブジェクトを生成したときの処理
+    // OnEnable()から呼び出す
+    protected virtual void Spawn()
     {
-        //
-    }
-
-    public void Spawn()
-    {
-        // オブジェクトを生成したときの処理
-        // instantiate自体はmanagerから呼び出す？
+        // スポーンの場所を指定
+        Vector2 spawnPosition = m_camera.ViewportToWorldPoint(new Vector2(1.0f, 0.5f));
+        m_rig.transform.position = spawnPosition;
     }
 
     private void Move()
@@ -53,32 +62,49 @@ public class Enemy : MonoBehaviour
     // プレイヤー側からの被ダメはプレイヤー自身で完結するため、アタックのモーションだけ実行する
     private void AttackPlayer()
     {
-        
+        // 攻撃のアニメーション
     }
 
     // 城に攻撃する
     private void AttackCastle()
     {
         // 城に衝突
-        // ゲームオーバー
+        // ゲームオーバーにする？
     }
 
-    // プレイヤーからのダメージを受ける
+    // プレイヤーからのダメージを受ける（Playerから呼び出し）
     public void TakeDamage(int ap)
     {
-        // 何かする
+        this.hp -= ap;
+        if (this.hp <= 0)
+        {
+            this.hp = 0;
+            Dead();
+        }
     }
 
+    public void Dead()
+    {
+        //  死んだときの動作をここに書く
+
+        // とりあえずGameObjectを削除
+        Destroy(this.gameObject);
+    }
+
+    // 衝突相手の判定はTagで
+    // Player or Castle
     public void OnCollisionEnter2D(Collision2D collision)
     {
         // 衝突時の動作
 
         GameObject opponent = collision.gameObject;    // 衝突相手を取得
-        if (true)    // あとで衝突相手がプレイヤーかを判定する条件を書く
+        Collider2D other = opponent.GetComponent<Collider2D>();
+
+        if (other.CompareTag("Player"))
         {
             AttackPlayer();
         }
-        else if (true)    // あとで衝突相手が城かを判定する条件を書く
+        else if (other.CompareTag("Castle"))
         {
             AttackCastle();
         }
