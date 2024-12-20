df70381688430b59bbfc82fd29b28f9383030e7d
Junpei Sano <s2010335@edu.cc.uec.ac.jp>
2024-12-20 13:15:39 +0900

    プレイヤーからダメージを受ける関数を作成

--- Assets/Sano/Enemy.cs
+++ Assets/Sano/Enemy.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    // とりあえずprivate
+    private int hp = 1;
+    private float speed = 1.0f;
+    private float attack = 1.0f;
 
-    public int hp = 1;
-    public float speed = 1.0f;
-    public float attack = 1.0f;
-
+    // Trueの間ターゲットされているマークを表示するようにする
     public bool isTargeting = false;
 
     private Rigidbody2D m_rig;
@@ -30,5 +31,12 @@ public class Enemy : MonoBehaviour
     {
         // ひとまず座標を直接更新する方式にする
         m_rig.position += new Vector2(-speed * Time.deltaTime, 0);
+    }
+
+    // プレイヤーからのダメージを受ける
+    public void TakeDamage(int ap)
+    {
+
+    
     }
 }
