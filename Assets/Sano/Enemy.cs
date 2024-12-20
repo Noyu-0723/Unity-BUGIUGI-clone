b986515e5a08aad898787b3fa03ed454959a89f7
Junpei Sano <s2010335@edu.cc.uec.ac.jp>
2024-12-20 13:32:07 +0900

    Playerなどとのアクセスにつかうpublic関数をいくつか追加

--- Assets/Sano/Enemy.cs
+++ Assets/Sano/Enemy.cs
@@ -2,12 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class Enemy : MonoBehaviour
 {
     // とりあえずprivate
     private int hp = 1;
     private float speed = 1.0f;
     private float attack = 1.0f;
+
+    // スポーンの場所
+    protected Vector2 spawnPosition;
 
     // Trueの間ターゲットされているマークを表示するようにする
     public bool isTargeting = false;
@@ -27,16 +32,55 @@ public class Enemy : MonoBehaviour
         Move();
     }
 
+    private void SetSpawnPosition()
+    {
+        //
+    }
+
+    public void Spawn()
+    {
+        // オブジェクトを生成したときの処理
+        // instantiate自体はmanagerから呼び出す？
+    }
+
     private void Move()
     {
         // ひとまず座標を直接更新する方式にする
         m_rig.position += new Vector2(-speed * Time.deltaTime, 0);
     }
 
+    // プレイヤーにアタックする
+    // プレイヤー側からの被ダメはプレイヤー自身で完結するため、アタックのモーションだけ実行する
+    private void AttackPlayer()
+    {
+        
+    }
+
+    // 城に攻撃する
+    private void AttackCastle()
+    {
+        // 城に衝突
+        // ゲームオーバー
+    }
+
     // プレイヤーからのダメージを受ける
     public void TakeDamage(int ap)
     {
+        // 何かする
+    }
 
-    
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        // 衝突時の動作
+
+        GameObject opponent = collision.gameObject;    // 衝突相手を取得
+        if (true)    // あとで衝突相手がプレイヤーかを判定する条件を書く
+        {
+            AttackPlayer();
+        }
+        else if (true)    // あとで衝突相手が城かを判定する条件を書く
+        {
+            AttackCastle();
+        }
     }
 }
